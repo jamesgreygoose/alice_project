9b1f89f74c95975902140454a9b3f5f49746dda6
jamesgreygoose <james@schoolofcode.co.uk>
2021-01-29 19:52:54 +0000

    Video files added

--- facts.js
+++ facts.js
@@ -5,7 +5,7 @@ let facts = [
   `"They run so fast because they are light and have long legs."`,
   `"They live in dry grassland in Africa."`,
   `"They are yellow and black so they can camouflage in the desert."`,
-  `"They are hunters or preditors."`,
+  `"They are hunters or predators."`,
   `"Cheetahs are hunted for their fur."`,
   `"They are the fastest cat on the planet."`,
   `"Cheetahs are the fastest land animal in the world."`,
